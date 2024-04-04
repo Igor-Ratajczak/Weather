962a08a3f889160b44586eae375d4bed97bf6b08
Igor Ratajczak <betelgeza6@gmail.com>
2024-04-04 10:59:55 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Weather/sw.js', { scope: '/Weather/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
