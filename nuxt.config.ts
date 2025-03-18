578b3c8f8b29c240c1ad88917fe4171059eac3d4
Igor Ratajczak <betelgeza6@gmail.com>
2025-03-18 15:52:10 +0100

    Change screenshots and increase limit cache size

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
   future: {
     compatibilityVersion: 4,
   },
@@ -15,6 +14,7 @@ export default defineNuxtConfig({
       globPatterns: ['**/*.{html,js,css,ico,png,svg,webmanifest,json}'],
       skipWaiting: false,
       clientsClaim: false,
+      maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
     },
     manifest: {
       name: 'Weather 2.0',
@@ -61,14 +61,28 @@ export default defineNuxtConfig({
       screenshots: [
         {
           src: './screenshot_home.png',
-          sizes: '520x520',
+          sizes: '1600x1200',
+          type: 'image/png',
+          form_factor: 'narrow',
+          label: 'Pogoda w Twoim mieście',
+        },
+        {
+          src: './screenshot_background.png',
+          sizes: '1600x1200',
+          type: 'image/png',
+          form_factor: 'narrow',
+          label: 'Tło dostosowane do Twojej aktualnej pogody',
+        },
+        {
+          src: './screenshot_home.png',
+          sizes: '1600x1200',
           type: 'image/png',
           form_factor: 'wide',
           label: 'Pogoda w Twoim mieście',
         },
         {
           src: './screenshot_background.png',
-          sizes: '520x520',
+          sizes: '1600x1200',
           type: 'image/png',
           form_factor: 'wide',
           label: 'Tło dostosowane do Twojej aktualnej pogody',
